9369eceede906aecbff98b1fba4382a7a01d42da
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-08-16 22:40:18 +0200

    add error catching

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
 const url = 'https://raw.githubusercontent.com/Chimoneg27/Time-Tracker/main/JSON/data.json';
+let days = [];
+
+fetch(url)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Could not fetch resource')
+        }
+        return response.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error)) //this catches an error since it is promise based
