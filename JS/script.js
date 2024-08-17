848da3c6eeb10332e04647096a66ef7c70e702aa
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-08-17 18:01:55 +0200

    add state for days week and month

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,8 @@
 const url = 'https://raw.githubusercontent.com/Chimoneg27/Time-Tracker/main/JSON/data.json';
 let cardContainer = document.getElementById('cards');
+const daily = document.getElementById('Daily');
+const weekly = document.getElementById('Weekly');
+const monthly = document.getElementById('Months')
 
 fetchData();
 async function fetchData() {
@@ -31,14 +34,68 @@ const activities = (arr, box) => {
 
         <ul class="duration">
           <li class='today'>${arr[i].timeframes.daily.current}hrs</li>
-          <li class='today'>Today - ${arr[i].timeframes.daily.previous}hrs</li>
-          <li class='week'>${arr[i].timeframes.weekly.current}hrs</li>
-          <li class='week'>${arr[i].timeframes.weekly.previous}hrs</li>
-          <li class='monthly'>${arr[i].timeframes.current}</li>
-          <li class='monthly'>${arr[i].timeframes.previous}</li>
+          <li class='today previous'>Yesterday - ${arr[i].timeframes.daily.previous}hrs</li>
+          <li class='week' style='color: white; font-size: 2.25rem;'>${arr[i].timeframes.weekly.current}hrs</li>
+          <li class='week' style='color: var(--Desaturated-blue);'>Last Week - ${arr[i].timeframes.weekly.previous}hrs</li>
+          <li class='monthly' style='color: white; font-size: 2.25rem;'>${arr[i].timeframes.monthly.current}hrs</li>
+          <li class='monthly' style='color: var(--Desaturated-blue);'>Last Month - ${arr[i].timeframes.monthly.previous}hrs</li>
         </ul>
       </div>
     </div>
   `
   }
 }
+
+daily.addEventListener('click', () => {
+  let today = document.querySelectorAll('.today');
+  let week = document.querySelectorAll('.week');
+  let monthly = document.querySelectorAll('.monthly');
+
+  for(let i = 0; i < today.length; i++) {
+    today[i].style.display = 'block';
+  }
+
+  for(let i = 0; i < week.length; i++) {
+    week[i].style.display = 'none';
+  }
+
+  for(let i = 0; i < monthly.length; i++) {
+    monthly[i].style.display = 'none';
+  }
+});
+
+weekly.addEventListener('click', () => {
+  let today = document.querySelectorAll('.today');
+  let week = document.querySelectorAll('.week');
+  let monthly = document.querySelectorAll('.monthly');
+
+  for(let i = 0; i < today.length; i++) {
+    today[i].style.display = 'none';
+  }
+
+  for(let i = 0; i < week.length; i++) {
+    week[i].style.display = 'block';
+  }
+
+  for(let i = 0; i < monthly.length; i++) {
+    monthly[i].style.display = 'none';
+  }
+});
+
+monthly.addEventListener('click', () => {
+  let today = document.querySelectorAll('.today');
+  let week = document.querySelectorAll('.week');
+  let monthly = document.querySelectorAll('.monthly');
+
+  for(let i = 0; i < today.length; i++) {
+    today[i].style.display = 'none';
+  }
+
+  for(let i = 0; i < week.length; i++) {
+    week[i].style.display = 'none';
+  }
+
+  for(let i = 0; i < monthly.length; i++) {
+    monthly[i].style.display = 'block';
+  }
+});
