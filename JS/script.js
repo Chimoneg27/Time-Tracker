728c06027773109d25137248e96566569c34fd64
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-08-17 16:37:38 +0200

    dynamically add daily times

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 const url = 'https://raw.githubusercontent.com/Chimoneg27/Time-Tracker/main/JSON/data.json';
-let days = [];
+let cardContainer = document.getElementById('cards')
 
 fetchData();
 async function fetchData() {
@@ -11,9 +11,49 @@ async function fetchData() {
       }
 
       const data = await response.json();
-      console.log(data);
+      const times = data.map(item => item.timeframes)
+      console.log(times)
+      // for(let i = 0; i < data.length; i++) {
+      //   cardContainer.innerHTML += `
+      //   <div class="activity">
+      //     <div class="cardTop"></div>
+      //       <ul class="task">
+      //         <li>${data[i].title}</li>
+      //         <li><img src="images/icon-ellipsis.svg" alt="icon-ellipsis"></li>
+      //       </ul>
+
+      //       <ul class="duration">
+      //         <li>32hrs</li>
+      //         <li>Last-week - 36hrs</li>
+      //       </ul>
+      //     </div>
+      //   </div>
+      // `
+      // }
+
+      activities(data, cardContainer)
     }
     catch(error) {
       console.log(error);
     }
+}
+
+const activities = (arr, box) => {
+  for(let i = 0; i < arr.length; i++) {
+    box.innerHTML += `
+    <div class="activity">
+      <div class="cardTop"></div>
+        <ul class="task">
+          <li>${arr[i].title}</li>
+          <li><img src="images/icon-ellipsis.svg" alt="icon-ellipsis"></li>
+        </ul>
+
+        <ul class="duration">
+          <li class='today'>${arr[i].timeframes.daily.current}hrs</li>
+          <li class='today'>Today - ${arr[i].timeframes.daily.previous}hrs</li>
+        </ul>
+      </div>
+    </div>
+  `
+  }
 }
